79b059ecbacd4aa8167beffb5ce56a11e1a30f37
jacky <jacky790125@gmail.com>
2019-05-20 13:24:36 +0800

    flot bug

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import $ from 'jquery'
+import flot from 'flot'
 
 window.$ = $;
+
+Vue.use(flot);
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <div id="msg"></div>
+    <div id="chart"></div>
   </div>
 </template>
 
@@ -11,7 +12,12 @@ import $ from 'jquery';
 export default {
     name: 'about',
     mounted() {
+      // jquery test
       $('#msg').text('test jquery');
+
+      // flot test
+      var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+      $.plot("#placeholder", d2);
     },
 }
 </script>
